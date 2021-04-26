273b8c279206dabc9d9b0a0aaaddaedb51675bef
joydeepmscs <joydeep.mscs@gmail.com>
2021-04-26 15:41:03 +0100

    Image controller redirection

--- src/screens/ImageViewerController.js
+++ src/screens/ImageViewerController.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, {Component, Fragment} from 'react'
+import {Route, Switch} from "react-router-dom"; // For redirection
 import Login from "./login/Login";
 import Home from './home/Home';
 import Profile from './profile/Profile';
 
 class ImageViewerController extends Component {
 
-  constructor() {
-    super();
-    this.baseUrl = "https://graph.instagram.com/";
-  }
+    constructor() {
+        super();
+        this.baseUrl = "https://graph.instagram.com/";
+    }
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <Route exact path='/' render={(props) => <Login {...props} />} />
-          <Route exact path='/home' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
-          <Route exact path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
-        </div>
-      </Router>
-    )
-  }
+    render() {
+        return (
+            <Fragment>
+                <Switch>
+                    <Route exact path='/' render={(props) => <Login {...props} />}/>
+                    <Route exact path='/home' render={(props) => <Home {...props} baseUrl={this.baseUrl}/>}/>
+                    <Route exact path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl}/>}/>
+                </Switch>
+            </Fragment>
+        )
+    }
 }
 
 export default ImageViewerController;
